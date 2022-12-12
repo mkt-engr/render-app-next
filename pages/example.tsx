7c2a89eabf0a539dadd9837ea9231637e75b07fe
Makito Mori <mkt-engr@gmail.com>
2022-12-12 23:53:27 +0900

    MUI使ってフォーム作成

--- pages/example.tsx
+++ pages/example.tsx
@@ -1,5 +1,31 @@
 import { GetStaticProps, NextPage } from "next";
 import axios from "axios";
+import Button from "@mui/material/Button";
+import {
+  Avatar,
+  Box,
+  Container,
+  Divider,
+  Grid,
+  IconButton,
+  InputBase,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Paper,
+  styled,
+  TextField,
+  Typography,
+} from "@mui/material";
+import FolderIcon from "@mui/icons-material/Folder";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import AddIcon from "@mui/icons-material/Add";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import DirectionsIcon from "@mui/icons-material/Directions";
+import React, { useState } from "react";
 
 type Task = {
   id: number;
@@ -12,20 +38,61 @@ type Task = {
 type Props = {
   tasks: Task[];
 };
-const Example: NextPage<Props> = ({ tasks }) => {
-  console.log(tasks, "in Example ");
 
+const Example: NextPage<Props> = ({ tasks }) => {
   return (
     <div>
-      <ul>
-        {tasks.map((task) => {
-          return (
-            <li key={task.id}>
-              {task.content}:{task.done ? "済" : "未"}
-            </li>
-          );
-        })}
-      </ul>
+      <Box sx={{ flexGrow: 1, maxWidth: 500, mx: "auto" }}>
+        <Grid container spacing={0}>
+          <Grid item xs={12} md={12}>
+            <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+              Please Add Your Task!!
+            </Typography>
+
+            <Paper
+              component="form"
+              sx={{
+                p: "2px 4px",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="New Task"
+                inputProps={{ "aria-label": "new task" }}
+              />
+              <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+                <AddIcon />
+              </IconButton>
+            </Paper>
+            <List>
+              {tasks.map((task) => {
+                return (
+                  <ListItem
+                    key={task.id}
+                    secondaryAction={
+                      <IconButton edge="end" aria-label="edit">
+                        <EditIcon />
+                      </IconButton>
+                    }
+                  >
+                    <ListItemAvatar>
+                      <Avatar>
+                        <FolderIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary={task.content} />
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 };
@@ -39,7 +106,6 @@ export const getServerSideProps: GetStaticProps = async () => {
   });
 
   const data = res.data;
-  console.log(data);
   return {
     props: {
       tasks: data,
