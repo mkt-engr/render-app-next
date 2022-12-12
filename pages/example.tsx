5015df038634c5e4d2911e88a5560c4367f533ad
Makito Mori <mkt-engr@gmail.com>
2022-12-13 02:41:26 +0900

    不要なインポート削除

--- pages/example.tsx
+++ pages/example.tsx
@@ -2,22 +2,14 @@ import { GetStaticProps, NextPage } from "next";
 import axios from "axios";
 import {
   Box,
-  Button,
   Checkbox,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
   FormControlLabel,
   Grid,
   IconButton,
   InputBase,
   List,
   ListItem,
-  ListItemText,
   Paper,
-  TextField,
   Typography,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
