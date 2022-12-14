1d19842bef35190cd6b8de4045f789e83da2bb4a
Makito Mori <mkt-engr@gmail.com>
2022-12-14 21:26:43 +0900

    タスク編集機能削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,10 +13,8 @@ import {
   Typography,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
-import EditIcon from "@mui/icons-material/Edit";
 import AddIcon from "@mui/icons-material/Add";
 import { SyntheticEvent, useState } from "react";
-import EditModal from "../components/EditModal";
 import { Task } from "../@types/type";
 
 type Props = {
@@ -26,9 +24,7 @@ type Props = {
 const Example: NextPage<Props> = ({ staticTasks }) => {
   const [tasks, setTasks] = useState(staticTasks);
   const [newTask, setNewTask] = useState("");
-  const [selectedTask, setSelectedTask] = useState(staticTasks[0]);
   const [deletedTaskId, setDeletedTaskId] = useState(0);
-  const [open, setOpen] = useState(false);
 
   const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
@@ -74,103 +70,80 @@ const Example: NextPage<Props> = ({ staticTasks }) => {
     });
   };
 
-  const handleClickOpen = (task: Task) => {
-    setSelectedTask(task);
-    setOpen(true);
-  };
-
   return (
-    <>
-      <Box sx={{ flexGrow: 1, maxWidth: 500, mx: "auto" }}>
-        <Grid container spacing={0}>
-          <Grid item xs={12} md={12}>
-            <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-              Please Add Your Task!! (Task limit is 10)
-            </Typography>
-            <form onSubmit={handleSubmit}>
-              <Paper
-                sx={{
-                  p: "2px 4px",
-                  display: "flex",
-                  alignItems: "center",
+    <Box sx={{ flexGrow: 1, maxWidth: 500, mx: "auto" }}>
+      <Grid container spacing={0}>
+        <Grid item xs={12} md={12}>
+          <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+            Please Add Your Task!! (Task limit is 10)
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <Paper
+              sx={{
+                p: "2px 4px",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="New Task"
+                inputProps={{ "aria-label": "new task" }}
+                value={newTask}
+                onChange={(e) => {
+                  setNewTask(e.target.value);
                 }}
+              />
+              <IconButton
+                type="button"
+                sx={{ p: "10px" }}
+                aria-label="search"
+                onClick={handleSubmit}
               >
-                <InputBase
-                  sx={{ ml: 1, flex: 1 }}
-                  placeholder="New Task"
-                  inputProps={{ "aria-label": "new task" }}
-                  value={newTask}
-                  onChange={(e) => {
-                    setNewTask(e.target.value);
-                  }}
-                />
-                <IconButton
-                  type="button"
-                  sx={{ p: "10px" }}
-                  aria-label="search"
-                  onClick={handleSubmit}
-                >
-                  <AddIcon />
-                </IconButton>
-              </Paper>
-              <List>
-                {tasks.map((task) => {
-                  return (
-                    <ListItem key={task.id}>
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            checked={task.done}
-                            value={task.id}
-                            onChange={handleTaskStatus}
-                          />
-                        }
-                        label={task.content}
-                        sx={{
-                          textDecoration: task.done ? "line-through" : "auto",
-                          wordBreak: "break-word",
-                          flex: 1,
-                          mr: 0,
+                <AddIcon />
+              </IconButton>
+            </Paper>
+            <List>
+              {tasks.map((task) => {
+                return (
+                  <ListItem key={task.id}>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={task.done}
+                          value={task.id}
+                          onChange={handleTaskStatus}
+                        />
+                      }
+                      label={task.content}
+                      sx={{
+                        textDecoration: task.done ? "line-through" : "auto",
+                        wordBreak: "break-word",
+                        flex: 1,
+                        mr: 0,
+                      }}
+                    />
+                    <Box sx={{ ml: "auto", mr: "4px" }}>
+                      <IconButton
+                        edge="end"
+                        aria-label="delete"
+                        onClick={() => {
+                          setDeletedTaskId(task.id);
+                          handleDelete(task.id);
                         }}
-                      />
-                      <Box sx={{ ml: "auto", mr: "4px" }}>
-                        <IconButton
-                          edge="end"
-                          aria-label="edit"
-                          onClick={() => {
-                            handleClickOpen(task);
-                          }}
-                        >
-                          <EditIcon />
-                        </IconButton>
-                        <IconButton
-                          edge="end"
-                          aria-label="delete"
-                          onClick={() => {
-                            setDeletedTaskId(task.id);
-                            handleDelete(task.id);
-                          }}
-                          disabled={task.id === deletedTaskId}
-                        >
-                          <DeleteIcon />
-                        </IconButton>
-                      </Box>
-                    </ListItem>
-                  );
-                })}
-              </List>
-            </form>
-          </Grid>
+                        disabled={task.id === deletedTaskId}
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                    </Box>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </form>
         </Grid>
-      </Box>
-
-      <EditModal
-        open={open}
-        setOpen={setOpen}
-        task={selectedTask}
-        setTasks={setTasks}
-      />
-    </>
+      </Grid>
+    </Box>
   );
 };
 
